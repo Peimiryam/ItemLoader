407bf7473351697fa8d13b9362522bc76347e2e9
Peimiryam <134741660+Peimiryam@users.noreply.github.com>
2023-06-16 20:26:36 +0100

    Add files via upload

---
+++ Itemloader.py
@@ -0,0 +1,65 @@
+
+print("Hello, this is your personal package loader. Read and follow the steps below:\n ")
+
+#Ask user for the number of item
+while True:
+    try:
+        number_of_items = int(input("Please enter the number of items you want to ship:\n "))
+    except ValueError:
+        print("Not a valid number. Try again. ")
+        continue
+    else:
+        break
+
+package_weight = 0
+
+package_sent = 0
+
+package_total_weight = 0
+
+#Ask user for the weight of each item
+for item in range (number_of_items):
+
+    item_weight = int(input("Please provide the weight of the item: "))
+
+
+#If the weight is not between 1 and 10 kg, print an error message and stop the program
+    if item_weight > 10 or item_weight < 1:
+            print("Item weight not supported")
+            break
+    #Check if the weight is between 1 and 10 kg
+    if item_weight >=1 or item_weight <= 10:
+        package_weight = package_weight + item_weight
+
+#If the weight is between 1 and 10 kg, add it to the package
+#If the weight is between 1 and 10 kg and the package is not full, add the weight to the package
+#If the weight is between 1 and 10 kg and the package is not full and it is the last item, send the package
+    if package_weight <= 20:
+              #this helps to understand how much weight and number of packages anytime the user adds
+        print(f"Weight: {package_weight}. Number of packages: {package_sent + 1}")
+        
+
+
+#If the weight is between 1 and 10 kg and the package is full, send the item and start a new one 
+    if package_weight > 20:
+            package_total_weight = package_total_weight + package_weight - item_weight
+            package_sent = package_sent + 1
+            print("Package exceeds 20 kg. We will prepare another package. First package sent, weight amount: \n" + str(package_total_weight))
+            package_weight = item_weight
+            package_sent = package_sent + 1
+#Print the number of sent packages
+#Print the weight of sent packages
+#Print the leftover weight
+#Print the package with the most empty space
+            print("Leftover weight: " + str(item_weight))
+            print(f"Total packages: {package_sent}")
+            print(f"Package with the most empty space: {package_weight}")
+            package_total_weight = package_total_weight + package_weight
+            print("Total: \n" + str(package_total_weight))
+            break
+
+
+print("Package Sent!")
+
+
+
